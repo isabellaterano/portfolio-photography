7e0b2f51569d617b58fe16722830d7c8f493425a
isabellaterano <isabellaiterano@gmail.com>
2024-08-16 09:57:51 -0300

    update deploy

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -19,22 +19,22 @@ export default function About() {
         <div className="md:w-2/3 md:pl-8 mt-6 md:mt-0">
           <h2 className="text-3xl font-semibold text-gray-800">About Me</h2>
           <p className="text-gray-600 mt-4">
-            Hi, I’m Jasper Everett, a passionate photographer with a love for
-            capturing life's most beautiful moments. I specialize in portrait,
-            landscape, and event photography. My journey into photography began
-            at a young age, and over the years, I’ve developed a unique style
-            that blends creativity with technical expertise.
+            Hi, I&apos;m Jasper Everett, a passionate photographer with a love
+            for capturing life&apos;s most beautiful moments. I specialize in
+            portrait, landscape, and event photography. My journey into
+            photography began at a young age, and over the years, I’ve developed
+            a unique style that blends creativity with technical expertise.
           </p>
           <p className="text-gray-600 mt-4">
             Through my lens, I aim to tell stories that resonate with people and
-            evoke emotions. Whether it’s a candid shot or a meticulously planned
-            portrait, I strive to create images that are not just visually
-            stunning but also meaningful.
+            evoke emotions. Whether it&apos;s a candid shot or a meticulously
+            planned portrait, I strive to create images that are not just
+            visually stunning but also meaningful.
           </p>
           <p className="text-gray-600 mt-4">
-            When I’m not behind the camera, I enjoy exploring new places,
+            When I&apos;m not behind the camera, I enjoy exploring new places,
             meeting new people, and continuously learning to hone my craft.
-            Let’s connect and create something beautiful together!
+            Let&apos;s connect and create something beautiful together!
           </p>
         </div>
       </div>
